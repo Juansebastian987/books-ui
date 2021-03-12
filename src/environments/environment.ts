7f4ee6b4038d6c6db376eedd5e54c68cbe51d78e
unknown <juanse.bastian2012@hotmail.com>
2021-03-12 18:27:49 -0500

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://integracion-continua-back.herokuapp.com/books',
+  baseUrl: 'https://integracion-continua-back.herokuapp.com',
 };
